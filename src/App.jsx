b4018d37546453d35a56eebb32e751ed7b6f12b2
Kpeale Legbara <66691866+kpeale@users.noreply.github.com>
2025-10-23 21:27:40 +0100

    implement showcase section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Nav from './components/Nav';
 import ProductViewer from './components/ProductViewer';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import gsap from 'gsap';
+import Showcase from './components/Showcase';
 gsap.registerPlugin(ScrollTrigger);
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Nav />
       <Hero />
       <ProductViewer />
+      <Showcase />
     </main>
   );
 }
