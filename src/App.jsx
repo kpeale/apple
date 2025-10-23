6006244d8eeb6530a25b4b95c6e3119dcbc8d694
Kpeale Legbara <66691866+kpeale@users.noreply.github.com>
2025-10-23 20:35:53 +0100

    implement product viewer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Hero from './components/Hero';
 import Nav from './components/Nav';
+import ProductViewer from './components/ProductViewer';
+import { ScrollTrigger, SplitText } from 'gsap/all';
+import gsap from 'gsap';
+gsap.registerPlugin(ScrollTrigger);
 function App() {
   return (
     <main>
       <Nav />
       <Hero />
+      <ProductViewer />
     </main>
   );
 }
