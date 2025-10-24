d83ffee1a9d4a4302fdbd8ff0eff082b315fe8a7
Kpeale Legbara <66691866+kpeale@users.noreply.github.com>
2025-10-24 11:41:45 +0100

    implement highlights section, features and performance section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import ProductViewer from './components/ProductViewer';
 import { ScrollTrigger, SplitText } from 'gsap/all';
 import gsap from 'gsap';
 import Showcase from './components/Showcase';
+import Performance from './components/Performance'
+import Features from './components/Features'
+import Highlights from './components/Highlights'
+import Footer from './components/Footer'
 gsap.registerPlugin(ScrollTrigger);
 function App() {
   return (
@@ -12,6 +16,10 @@ function App() {
       <Hero />
       <ProductViewer />
       <Showcase />
+      <Performance/>
+      <Features/>
+      <Highlights/>
+      <Footer/>
     </main>
   );
 }
